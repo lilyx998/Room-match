e3e4c1eb4caa4eaeb2e6f86ea4d7db8190957ab3
Lily Yang <lilyyang@fb.com>
2022-07-08 16:39:00 -0700

    Added objects in view profile screen to storyboard

---
+++ Roommatch/Roommatch/ViewControllers/ViewProfileViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewProfileViewController.h
+//  Roommatch
+//
+//  Created by Lily Yang on 7/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewProfileViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Roommatch/Roommatch/ViewControllers/ViewProfileViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewProfileViewController.m
+//  Roommatch
+//
+//  Created by Lily Yang on 7/8/22.
+//
+
+#import "ViewProfileViewController.h"
+
+@interface ViewProfileViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *ageLabel;
+@property (weak, nonatomic) IBOutlet UILabel *pronounsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *locationLabel;
+@property (weak, nonatomic) IBOutlet UILabel *priceRangeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bioLabel;
+@property (weak, nonatomic) IBOutlet UILabel *smokingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *petsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *inCollegeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *collegeNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *instagramLabel;
+
+@end
+
+@implementation ViewProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
