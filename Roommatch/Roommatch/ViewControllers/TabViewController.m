5331d793f2f3c8d9fbcc0558829c568b83bd2df6
Lily Yang <lilyyang@fb.com>
2022-07-08 10:46:32 -0700

    Implemented logout

---
+++ Roommatch/Roommatch/ViewControllers/TabViewController.m
@@ -0,0 +1,46 @@
+//
+//  TabViewController.m
+//  Roommatch
+//
+//  Created by Lily Yang on 7/8/22.
+//
+
+#import "TabViewController.h"
+#import <Parse/Parse.h>
+#import "User.h"
+
+@interface TabViewController ()
+
+@end
+
+@implementation TabViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)logout:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        // PFUser.current() will now be nil
+        if(error){
+            NSLog(@"☹️☹️☹️ Couldn't log out: %@", error.localizedDescription);
+        }
+        else{
+            NSLog(@"😇😇😇 Logout success!");
+        }
+    }];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
